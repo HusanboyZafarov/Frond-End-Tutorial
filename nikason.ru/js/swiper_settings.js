c5c11faef7cc80bcefe2a8d7e5beec22d5b815ef
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2022-12-03 22:04:14 +0500

    adding swiper js

---
+++ nikason.ru/js/swiper_settings.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
---
+++ py.py
@@ -0,0 +1,4 @@
+num = 1
+arr = [1, 2, 3, 4, 5, 6]
+if num in arr:
+    print("hello")
