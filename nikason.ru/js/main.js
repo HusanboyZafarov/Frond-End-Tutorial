4779e2e00a4ef0145a69cca6f13cec5051efefb8
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2022-12-04 14:47:58 +0500

    creating about html

--- nikason.ru/js/main.js
+++ nikason.ru/js/main.js
@@ -24,4 +24,21 @@ for (let i = 0; i < header_item.length; i++) {
     header_list.classList.toggle("opened")
   })
 }
-
+let form_btn = document.querySelector(".form_btn"),
+  message_for_user = document.querySelector(".message_for_user"),
+  form_wrapper = document.querySelector(".form_wrapper"),
+  my_form = document.querySelector(".my_form"),
+  opener_btn = document.querySelector(".opener_btn")
+opener_btn.addEventListener("click", () => {
+  form_wrapper.classList.toggle("opened")
+})
+form_btn.addEventListener("click", () => {
+  if (my_form.valid()) {
+    message_for_user.style.display = "block"
+  }
+  form_wrapper.classList.add("closed", my_form.valid())
+  setTimeout(closed_func, 2000)
+  function closed_func() {
+    message_for_user.classList.add("closed")
+  }
+})
